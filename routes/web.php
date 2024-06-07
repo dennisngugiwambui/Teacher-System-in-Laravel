2264c13afea956ab18c6ffcc801e9b3bca59fe2f
Denno <dellyit001@gmail.com>
2024-06-07 00:30:30 -0700

    jwt auth route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::post('register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+
+Route::get('logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+
+//// Post
+//Route::post('posts/create', [App\Http\Controllers\Api\PostController::class, 'create'])->middleware('jwtAuth');
+//Route::post('posts/edit', [App\Http\Controllers\Api\PostController::class, 'update'])->middleware('jwtAuth');
+//Route::post('posts/delete', [App\Http\Controllers\Api\PostController::class, 'delete'])->middleware('jwtAuth');
+//Route::get('posts', [App\Http\Controllers\Api\PostController::class, 'posts'])->middleware('jwtAuth');
+//
+//// Comments
+//Route::post('comments/create', [App\Http\Controllers\Api\CommentController::class, 'create'])->middleware('jwtAuth');
+//Route::post('comments/edit', [App\Http\Controllers\Api\CommentController::class, 'update'])->middleware('jwtAuth');
+//Route::post('comments/delete', [App\Http\Controllers\Api\CommentController::class, 'delete'])->middleware('jwtAuth');
+//Route::get('comments', [App\Http\Controllers\Api\CommentController::class, 'comments'])->middleware('jwtAuth');
+
