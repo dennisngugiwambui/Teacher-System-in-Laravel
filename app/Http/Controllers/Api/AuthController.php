c9f671be3975f3056c158ea8cc03806899a841e6
Denno <dellyit001@gmail.com>
2024-06-07 00:26:07 -0700

    jwt auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Exception;
+use Illuminate\Support\Facades\Log;
+
+
 class AuthController extends Controller
 {
     //
