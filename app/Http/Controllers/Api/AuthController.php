22228c49694da93b05a47ec03cf4bcbb315ddc34
Denno <dellyit001@gmail.com>
2024-06-07 00:28:54 -0700

    jwt auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,5 +15,80 @@ use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        try {
+            $credentials = $request->only(['email', 'password']);
+
+
+            if (!$token = Auth::attempt($credentials)) {
+                return response()->json([
+                    'success' => false
+                ]);
+            }
+
+
+            // Generate an expiration time for the token (e.g., 1 hour)
+            $expiration = now()->addHours(1)->timestamp;
+
+
+            return response()->json([
+                'success' => true,
+                'token' => $token,
+                'expires_at' => $expiration,
+                'user' => Auth::user(),
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'invalid credentials',
+            ]);
+        }
+    }
+
+
+    public function register(Request $request)
+    {
+        $encryptedPassword = Hash::make($request->password);
+
+
+        $user = new User;
+
+
+        try {
+            $user->username = $request->username;
+            $user->phone = $request->phone;
+            $user->email = $request->email;
+            $user->password = $encryptedPassword;
+            $user->save();
+
+
+            // return $this->login($request);
+            return redirect()->route('username', ['username' => $user->username])->with('success', 'user logged in successfully');
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e,
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            // Invalidate the token instead of parsing it
+            Auth::logout();
+
+
+            return response()->json(['success' => true]);
+        } catch (Exception $e) {
+            Log::error('Error invalidating token: ' . $e->getMessage());
+            return response()->json(['error' => 'Could not parse token']);
+        }
+    }
 }
+
+
+
+
+
