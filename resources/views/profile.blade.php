3abdfb04bc4fda633d3ca70dc1e3a562d9577b46
Denno <dellyit001@gmail.com>
2024-06-17 03:51:51 -0700

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,8 +12,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{asset('Teacher/img/icons/icon-48x48.png')}}" />
 
-    <link rel="canonical" href="https://demo-basic.adminkit.io/" />
-
     <title>Teacher Dashboard</title>
 
     <link href="{{asset('Teacher/css/app.css')}}" rel="stylesheet">
@@ -58,7 +56,99 @@
 
 <body>
 <div class="wrapper">
-    @include('sidebar')
+    <nav id="sidebar" class="sidebar js-sidebar">
+        <div class="sidebar-content js-simplebar">
+            <a class="sidebar-brand" href="{{route('home')}}">
+                <span class="align-middle">Teacher Dashboard</span>
+            </a>
+
+            <ul class="sidebar-nav">
+                <li class="sidebar-header">
+                    Pages
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{route('home')}}">
+                        <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item active">
+                    <a class="sidebar-link" href="{{route('profile')}}">
+                        <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-sign-in.html">
+                        <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-sign-up.html">
+                        <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-blank.html">
+                        <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-header">
+                    Tools & Components
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-buttons.html">
+                        <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-forms.html">
+                        <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-cards.html">
+                        <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-typography.html">
+                        <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="icons-feather.html">
+                        <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-header">
+                    Plugins & Addons
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="charts-chartjs.html">
+                        <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="maps-google.html">
+                        <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="main">
         <nav class="navbar navbar-expand navbar-light navbar-bg">
