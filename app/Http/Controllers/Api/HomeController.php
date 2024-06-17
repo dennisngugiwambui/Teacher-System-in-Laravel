029bcf021bbf5abd13636c2f74c9dfa6ffe2af5f
Denno <dellyit001@gmail.com>
2024-06-17 03:58:43 -0700

    profile

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Teacher;
+use Brian2694\Toastr\Toastr;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function profile(Request $request)
+    {
+        $Teacher = Teacher::where('unique_id', $unique_id)->first();
+
+        // Check if the teacher exists
+        if (!$Teacher) {
+            Toastr::error('error trying to load profile!!','error!');
+            return response()->json(['error' => 'Teacher not found'], 404);
+        }
+
+        return view('profile',  ['teacher' => $Teacher]);
+    }
 }
